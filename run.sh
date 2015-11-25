da7829b9c6c5562ad19a490cfeb9833a883e8443
Milo van der Linden <milo@dogodigi.net>
2015-11-25 11:48:01 +0100

    Using environment variables for DB connection
    Use the tutum variables to define the database connection. Run
    install_dev instead of prod.

--- run.sh
+++ run.sh
@@ -6,15 +6,15 @@ else
     a2enmod rewrite
 fi
 chown www-data:www-data /webdir -R
-chmod a+x /webdir/install_prod.sh
+chmod a+x /webdir/install_dev.sh
 
 sed -i "s/_DATABASE_NAME/$MYSQL_DATABASE/g" /webdir/app/config/parameters.yml
-sed -i "s/_DATABASE_HOST/$MYSQL_HOST/g" /webdir/app/config/parameters.yml
-sed -i "s/_DATABASE_PORT/$MYSQL_PORT/g" /webdir/app/config/parameters.yml
-sed -i "s/_DATABASE_USER/$MYSQL_USER/g" /webdir/app/config/parameters.yml
-sed -i "s/_DATABASE_PASS/$MYSQL_PASSWORD/g" /webdir/app/config/parameters.yml
+sed -i "s/_DATABASE_HOST/$DB_PORT_3306_TCP_ADDR/g" /webdir/app/config/parameters.yml
+sed -i "s/_DATABASE_PORT/$DB_PORT_3306_TCP_PORT/g" /webdir/app/config/parameters.yml
+sed -i "s/_DATABASE_USER/$DB_ENV_MYSQL_USER/g" /webdir/app/config/parameters.yml
+sed -i "s/_DATABASE_PASS/$DB_ENV_MYSQL_PASS/g" /webdir/app/config/parameters.yml
 
-/webdir/install_prod.sh
+/webdir/install_dev.sh
 
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
